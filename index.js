7bc3c37f99911f93bb0959e8b945936a44caf7bb
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-23 18:39:50 +0530

    updated files

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const mysql = require('mysql2');
 const express = require ("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.json());
+
+app.use(express.urlencoded({extended : true}));
 app.set("view engine","ejs");
 app.set("views" , path.join(__dirname, "/views"));
 
@@ -59,19 +64,45 @@ app.get("/user",(req,res) =>{
 });
 
 app.get("/user/:id/edit", (req,res) =>{
-  
+  let {id} = req.params;
+  let q = `select * from emp where id ='${id}'`;
+  try{
+    connection.query(q,(err,result) =>{
+      if(err) throw err;
+      let data = result[0];
+      res.render("edit.ejs",{data});
+    });
+  }catch(err){
+    res.send("some error occured");
+  }
+});
+
+// Update Route
+app.patch("/user/:id" ,(req,res) =>{
+  let {id} = req.params;
+  let { password: formPass, username:newUsername} = req.body;
+  let q = `select * from emp where id ='${id}'`;
+  try{
+    connection.query(q, (err,result) =>{
+      if(err) throw err;
+      let data = result[0];
+      if(formPass!= data.password){
+        res.send("WRONG password");
+      }else{
+        let q2 = `update emp set username = '${newUsername}' where id = '${id}'`
+        connection.query(q2 , (err,result) =>{
+          if(err) throw err;
+          res.redirect('/user');
+        });
+      }
+     
+    });
+  }catch(err){
+    res.send("some error occured");
+  }
 });
 
 app.listen("8080" , () =>{
   console.log("Server is listening to port 8080");
 });
 
-// try{
-//     connection.query(q,[data],(err,result) =>{
-//     if(err) throw err;
-//     console.log(result);      
-// });
-// }catch(err){
-//     console.log(err);
-// }
-// connection.end();
