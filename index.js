f91ffcfa398eba20cef3a0b9b3a98855c81a30f1
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-23 08:26:07 +0530

    updated files

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require ("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.use("views" , path.join(__dirname, "/views"));
+
+
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -19,29 +25,31 @@ let createRandomUser = () => {
   ];
 };
 
-let q = "INSERT INTO emp(id,username,email,password) VALUES ?";
-
-let data = [];
-for(let i=1; i<=100; i++){
-    data.push(createRandomUser());
-}
-
-try{
-    connection.query(q,[data],(err,result) =>{
+// fetch and show total number of users on our app
+app.get("/" , (req,res) =>{
+  let q = "select count(*) from emp";
+  try{
+    connection.query(q,(err,result) =>{
     if(err) throw err;
-    console.log(result);      
+    console.log(result[0]["count(*)"]);     
+    res.send(result[0]["count(*)"]); 
 });
 }catch(err){
     console.log(err);
+    res.send("Some Error in database");
 }
-connection.end();
-
-app.get("/" , (req,res) =>{
-  res.send("Welcome to home page");
-
 });
 
 app.listen("8080" , () =>{
   console.log("Server is listening to port 8080");
-
 });
+
+// try{
+//     connection.query(q,[data],(err,result) =>{
+//     if(err) throw err;
+//     console.log(result);      
+// });
+// }catch(err){
+//     console.log(err);
+// }
+// connection.end();
