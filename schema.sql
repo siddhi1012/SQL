d5ec9c903bb3796ab477dbea9f8499700ca31689
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-23 07:55:23 +0530

    updated files

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-create table user(
-    id varchar(30) primary key,
-    username varchar(30) unique,
-    email varchar(30) unique not null,
-    password varchar(30) not null
+create table emp(
+    id varchar(50) primary key,
+    username varchar(50) unique,
+    email varchar(50) unique not null,
+    password varchar(50) not null
 );
